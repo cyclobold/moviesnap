260055d42c48016e81f04956a6fbe9b8c03d6218
Olu <theoafactor@gmail.com>
2023-04-26 12:02:58 +0100

    Added to Favs added

--- src/App.js
+++ src/App.js
@@ -15,8 +15,68 @@ class App extends Component{
         this.moviesResult = []
 
         this.state = {
-            movies: []
+            movies: [],
+            favourites_movies: this.getCurrentStorageCount()
         }
+
+
+    }
+
+    getCurrentStorageCount(){
+        let fav_movies_storage = localStorage.getItem("movies_favourites");
+
+        if(fav_movies_storage == null || typeof fav_movies_storage == undefined){
+            return 0;
+        }
+
+        const fav_movies_array = JSON.parse(fav_movies_storage);
+
+        return fav_movies_array.length;
+    }
+
+
+    addToFavourites(event, movie, index){
+
+      console.log(event.target.innerHTML)
+      // alert(index)
+      //console.log(movie)
+
+      event.target.classList.remove("btn-outline-secondary");
+      event.target.classList.add("btn-outline-danger");
+      event.target.innerHTML = "Added to Favourites";
+
+      //proceed to adding to locaStorage
+      let fav_movies_storage = localStorage.getItem("movies_favourites");
+
+      movie["custom_index"] = index;
+
+      if(fav_movies_storage == null || typeof fav_movies_storage == undefined){
+        let fav_movies_array = [];
+        fav_movies_array.push(movie)
+        //there is no storage
+        localStorage.setItem("movies_favourites", JSON.stringify(fav_movies_array));
+
+  
+        //update the state by 1
+        this.setState((prevState) => ({
+            favourites_movies: prevState.favourites_movies + 1
+        }))
+
+      }else{
+          //there is now a storage
+          let fav_movies_array = JSON.parse(fav_movies_storage);
+
+          fav_movies_array.push(movie)
+
+          localStorage.setItem("movies_favourites", JSON.stringify(fav_movies_array));
+
+          //update the state by 1
+          this.setState((prevState) => ({
+            favourites_movies: prevState.favourites_movies + 1
+          }))
+
+
+      }
 
 
     }
@@ -25,7 +85,7 @@ class App extends Component{
 
         axios.get("http://www.omdbapi.com/?s=Spiderman&apikey=81e3fca8").then((results) => {
 
-        console.log(results)
+        //console.log(results)
 
         this.setState({
             movies: results.data.Search
@@ -42,8 +102,7 @@ class App extends Component{
             <p class="card-text">{movie.Title}</p>
             <div class="d-flex justify-content-between align-items-center">
               <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" onClick={(event) => this.addToFavourites(event, movie, index)}>Add to Favourites</button>
               </div>
               <small class="text-muted">9 mins</small>
             </div>
@@ -60,37 +119,31 @@ class App extends Component{
 
      
     return <div>
-                   <header>
-  <div class="collapse bg-dark" id="navbarHeader">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-8 col-md-7 py-4">
-          <h4 class="text-white">About</h4>
-          <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
-        </div>
-        <div class="col-sm-4 offset-md-1 py-4">
-          <h4 class="text-white">Contact</h4>
-          <ul class="list-unstyled">
-            <li><a href="#" class="text-white">Follow on Twitter</a></li>
-            <li><a href="#" class="text-white">Like on Facebook</a></li>
-            <li><a href="#" class="text-white">Email me</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
+<header class="">
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <a class="navbar-brand" href="#">MoviesBase</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      {this.state.favourites_movies} favourites
+    </span>
   </div>
-  <div className="navbar navbar-dark bg-dark shadow-sm">
-    <div className="container d-flex justify-content-between">
-      <a href="#" class="navbar-brand d-flex align-items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="mr-2" viewBox="0 0 24 24" focusable="false"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
-        <strong>Movies Base</strong>
-      </a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-    </div>
-  </div>
+</nav>        
 </header>
+ 
 
 <main role="main">
 
